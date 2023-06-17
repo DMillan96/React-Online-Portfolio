8d580ce1fcebd8c1def3fbd473e87723a5d0ff9e
DMillan96 <danmil114@gmail.com>
2023-06-16 22:52:24 -0400

    saving progress

---
+++ src/components/Resume.js
@@ -0,0 +1,6 @@
+import TrackVisibility from "react-on-screen";
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+
+export const Resume = () => {
+  return <section className="resume" id="resume"></section>;
+};
